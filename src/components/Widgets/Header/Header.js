d16589f75e087a072e3525f2e20da785b25975fd
montygoldy <montygoldy2006@gmail.com>
2018-04-18 23:42:09 -0400

    Added reusuable header component with switch statement for different header template for the pages

--- src/components/Widgets/Header/Header.js
+++ src/components/Widgets/Header/Header.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
-const Header = () => {
+const RenderHeader = ({ headerType }) => {
+    let template = null;
+    // Switch statement to use different header for 3 pages
+    switch (headerType) {
+      case "logo":
+        template = <img src="/image/logo/logo.png" alt="logo" className="logo-image" />
+        break;
+    
+      case "venues":
+        template = <h1>venuesHeader</h1>
+        break;
+
+      case "venueDetail":
+        template = <h1>venueDetail</h1>
+        break;
+
+      default:
+        template = null;
+        break;
+    }
+    return template;
+}
+
+const Header = ({ type }) => {
   return (
-    <div>
-      Header
-    </div>
+    <header className="header">
+      <RenderHeader headerType={type} />
+    </header>
   );
 }
 
