bbc8a77f75524e4baa9b8941dc72b6af769b492a
montygoldy <montygoldy2006@gmail.com>
2018-04-18 21:50:45 -0400

    Footer styling finished

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <React.Fragment>
+      Test
+    </React.Fragment>
+  )
 };
 
 export default Home;
--- src/components/Widgets/Footer/Footer.js
+++ src/components/Widgets/Footer/Footer.js
@@ -2,15 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <footer className="footer">
+  return <footer className="footer">
       <ul className="footer__list">
-        <li className="footer__listItem"><Link to="/about">About Us</Link></li>
-        <li className="footer__listItem"><Link to="/contact">Contact</Link></li>
-        <li className="footer__listItem"><Link to="/blog">Blog</Link></li>
+        <li className="footer__listItem">
+          <Link to="/about" className="footer__itemLink">
+            About Us
+          </Link>
+        </li>
+        <li className="footer__listItem">
+          <Link to="/contact" className="footer__itemLink">
+            Contact
+          </Link>
+        </li>
+        <li className="footer__listItem">
+          <Link to="/blog" className="footer__itemLink">
+            Blog
+          </Link>
+        </li>
       </ul>
-    </footer>
-  );
+    </footer>;
 }
 
 export default Footer;
