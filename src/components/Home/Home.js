7ac52096e4286f45efd55a4e8a092df525955399
montygoldy <montygoldy2006@gmail.com>
2018-04-21 16:13:54 -0400

    Lifted search form values to homepage inorder to make api calls

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,29 @@ import React, { Component } from "react";
 import Header from "../Widgets/Header/Header";
 import SearchForm from "../Widgets/SearchForm/SearchForm";
 class Home extends Component {
+  constructor(props){
+    super(props);
 
+    this.state = {
+      res: []
+    }
+  }
+
+  venueSearch = (place, location) => {
+    console.log(place, location);
+    const api_id = process.env.REACT_APP_CLIENT_ID;
+    const api_key = process.env.REACT_APP_CLIENT_SECRET;
+  }
+  
+  
   render() {
     return (
       <div className="pageContainer">
-        <Header type="logo" />
+        <Header type="home" />
         <main className="content">
           <h2 className="main-text u-margin-bottom-s">Lorem Ipsum dolor sit!</h2>
           <span className="secondary-text u-margin-bottom-m">Lorem Ispsum dolor sit amet, consectetor adipiscing elit,<br /> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
-          <SearchForm />
+          <SearchForm onSearch={this.venueSearch} />
         </main>
       </div>
     );
--- src/components/Widgets/SearchForm/SearchForm.js
+++ src/components/Widgets/SearchForm/SearchForm.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SearchForm = () => {
-  return (
-    <form>
-      <input type="text" name="place" className="place" placeholder="I'm looking for" />
-      <input type="text" name="location" className="location" placeholder="Istanbul" />
-      <button><img src="/image/search.png" alt="search icon" /></button>
-    </form>
-  )
+class SearchForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      place: "",
+      location: ""
+    }
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState ({
+      [name]: value
+    })   
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { place, location } = this.state;
+    this.props.onSearch(place, location);
+  }
+
+  render() {
+    return (
+      <form action="">
+        <input type="text" name="place" onChange={this.handleChange} value={this.place} className="place" placeholder="I'm looking for" />
+        <input type="text" name="location" onChange={this.handleChange} value={this.location} className="location" placeholder="Location" />
+        <button onClick={this.handleSubmit} ><img src="/image/search.png" alt="search icon" /></button>
+      </form>
+    )
+  }
 }
 
 export default SearchForm;
